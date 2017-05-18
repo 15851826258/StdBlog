9478b1bb5305d4d81e46d075ba1f4e2bdbfc6ae4
张良 <562974055@qq.com>
2017-05-18 16:23:21 +0800

    fin

--- StdBlog/Controllers/HomeController.cs
+++ StdBlog/Controllers/HomeController.cs
@@ -18,17 +18,13 @@ namespace StdBlog.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Pg_wb()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Pg_dnc()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
